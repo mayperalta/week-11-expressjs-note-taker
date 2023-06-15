db12a2e41359f3393b29017cb94bd22585102d49
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-06-15 10:06:01 -0400

    Revised api.js

--- routes/html.js
+++ routes/html.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const path = require('path'); 
 
+// points to the public folder
+router.use(express.static('public'));
+
 // GET /notes and return /notes.html
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));

0e1a8b0b6dfc07aff9def06503139ddaa80fa85b
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-06-17 20:22:33 -0400

    Added code for api.js

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const fs = require ('fs'); 
+const uuid = require('../helper/uuid');
+// const { v4: uuidv4 } = require('uuid');
 
 let data = require('../db/db.json');
+let dbPath =  './db/db.json';
 
 // path - sends static assets
 //API or CONTROLLER routes interact with fetch()
@@ -10,37 +13,67 @@ let data = require('../db/db.json');
 // GET /api/notes` should read the `db.json` file and return all saved notes as JSON
 router.get('/notes', (req, res) => {
     res.json(data)
-})
+});
 
+// POST a new note and add it to db.json
+router.post('/notes', (req, res) => {
 
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If title and text exists
+    if (title && text) {
+        // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        id: uuid()
+      };
+    // import data and append new note
+    data.push(newNote);
 
+    let dataString = JSON.stringify(data, null, 3);
 
+    fs.writeFile(
+        dbPath,
+        dataString,
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully added the note!')
+    )
 
-// POST /api/notes  
-// 1 receive new note to ave on request body
-// 2 add new note to db.json
-// 3 return new note to client
+    const response = {
+        status: 'success',
+        body: newNote,
+      };
 
-// router.post("/notes", (req, res)=>{
-//     let newNote ={
-//         title: req.body.title,
-//         text: req.body.text
-//     }
-//     data.push(newNote)
-//     //use fs.writeFileSync to rewrite dbjson with new data included
-//     fs.writeFile('../db/dib.json', newNote => error ? console.log(error):console.log("Success!"));
+    console.log(response);
+    res.status(201).json(response);
 
-//    // res.json(data)
-//    res.json(data)
-
-// })
-
+    } else {
+    res.status(500).json('Error in posting note');
+    }
+  
+  });
 
 // DELETE /api/notes/:id 
 // 1 receive query parameter that contains note id to delete
 // 2 to deletem read all notes from db.json file
 // 3 remove note with the given id property
 // 4 rewrite the notes to the db.json
+
+// router.delete('/notes/:id', (req, res) => {
+
+//     fs.readFile( dbPath, 'utf8', (err, data) => {
+//         if (err) {
+//             console.error(err);
+//           } else {
+//             data = JSON.parse(data)
+//           }
+//     })
+
+// })
 
 
 module.exports = router; 
